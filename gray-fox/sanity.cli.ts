d9ceb0b69971877712392b4c849ae14486344020
MateoBaltar <mateobaltar@gmail.com>
2023-04-16 17:54:28 -0300

    First commit

---
+++ gray-fox/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hlf7xb0m',
+    dataset: 'production'
+  }
+})
---
+++ gray-fox/schemas/index.ts
@@ -0,0 +1,5 @@
+import producto from "./producto";
+import banner from "./banner";
+
+
+export const schemaTypes = [producto, banner]
